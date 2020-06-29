f55434bab1e7cb537b3730db6f05b72a6c39dfd1
Aleksandr <medovnikaleksander@yandex.ua>
2020-06-29 17:14:04 +0300

    First

---
+++ main.js
@@ -0,0 +1,38 @@
+"use strict"
+
+const canvas = document.getElementById('canvas');
+const ctx = canvas.getContext('2d');
+canvas.width = document.documentElement.clientWidth;
+canvas.height = document.documentElement.clientHeight;
+
+
+
+let start, anim;
+
+function playGame() {
+    start = true;
+    init(); // иннициализация переменных
+    game(); // запуск анимации
+};
+function init() {
+    start = true;
+};
+function game() {
+    if (start) {
+        update();
+        render();
+        anim = requestAnimationFrame(game);
+    }
+};
+function render(){
+
+};
+function update(){
+
+};
+
+
+
+
+
+window.onload = () => playGame(); 
