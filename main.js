737d35d04cb4f98573d78805310e22dc642f8bd9
Aleksandr Medovnik <45307269+AleksMedovnik@users.noreply.github.com>
2020-06-29 18:58:56 +0300

    Update main.js

--- main.js
+++ main.js
@@ -9,16 +9,25 @@ const ballImage = new Image();
 ballImage.src = 'images/ball.png';
 
 
-let start, anim;
+let start, anim, ball;
 
 function playGame() {
     start = true;
     init(); // иннициализация переменных
     game(); // запуск анимации
 };
+
 function init() {
     start = true;
+    ball = {
+        x: 50,
+        y: 50,
+        w: 55,
+        h: 55,
+    };
+    document.addEventListener('click', rebound);
 };
+
 function game() {
     if (start) {
         update();
@@ -26,15 +35,67 @@ function game() {
         anim = requestAnimationFrame(game);
     }
 };
-function render(){
-    ctx.drawImage(ballImage, 100, 400, 55, 55);
+
+function render() {
+    // background
+    ctx.save();
+    ctx.fillStyle = 'rgba(36, 50, 156, 1)';
+    ctx.fillRect(0, 0, canvas.width, canvas.height);
+    ctx.restore();
+
+    // ball
+    ctx.drawImage(ballImage, ball.x, ball.y, ball.w, ball.h);
 };
-function update(){
+
+function update() {
 
 };
 
 
 
 
+function makeEaseOut(timing) {
+    return function (timeFraction) {
+        return 1 - timing(1 - timeFraction);
+    }
+};
+
+function bounce(timeFraction) {
+    for (let a = 0, b = 1; true; a += b, b /= 2) {
+        if (timeFraction >= (7 - 4 * a) / 11) {
+            return -Math.pow((11 - 6 * a - 11 * timeFraction) / 4, 2) + Math.pow(b, 2)
+        }
+    }
+};
+
+function rebound() {
+    let to = canvas.height - ball.h;
+    animate({
+        duration: 2000,
+        timing: makeEaseOut(bounce),
+        draw(progress) {
+            ball.y = to * progress;
+        }
+    });
+};
+
+function animate(options) {
+    let start = performance.now();
+    requestAnimationFrame(function animate(time) {
+        // timeFraction от 0 до 1
+        let timeFraction = (time - start) / options.duration;
+        if (timeFraction > 1) timeFraction = 1;
+
+        // текущее состояние анимации
+        let progress = options.timing(timeFraction)
+
+        options.draw(progress);
+
+        if (timeFraction < 1) {
+            requestAnimationFrame(animate);
+        }
+
+    });
+};
 
 window.onload = () => playGame(); 
