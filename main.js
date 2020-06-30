4fd8b676bcee5dd01660f84d8b1fad52895070eb
Aleksandr Medovnik <45307269+AleksMedovnik@users.noreply.github.com>
2020-06-30 13:44:17 +0300

    Update main.js

--- main.js
+++ main.js
@@ -7,9 +7,11 @@ canvas.height = document.documentElement.clientHeight;
 
 const ballImage = new Image();
 ballImage.src = 'images/ball.png';
+const ringImage = new Image();
+ringImage.src = 'images/ring.png';
 
 
-let start, anim, ball;
+let start, anim, ball, platform, ring;
 
 function playGame() {
     start = true;
@@ -20,12 +22,32 @@ function playGame() {
 function init() {
     start = true;
     ball = {
-        x: 500,
+        x: 50,
         y: 50,
+        // y: canvas.height - 105,
         w: 55,
         h: 55,
+        dx: 10,
+        dy: 5,
+        g: 1.06,
+        start: false,
+
     };
+    platform = {
+        x: 0,
+        y: canvas.height - 50,
+        w: canvas.width,
+        h: 100
+    };
+    ring = {
+        x: canvas.width - 400,
+        y: 100,
+        w: 400,
+        h: 200
+    }
+    // document.addEventListener('click', rebound);
     document.addEventListener('click', rebound);
+
 };
 
 function game() {
@@ -45,15 +67,37 @@ function render() {
 
     // ball
     ctx.drawImage(ballImage, ball.x, ball.y, ball.w, ball.h);
+
+    // platform
+    ctx.save();
+    ctx.fillStyle = 'rgba(10, 74, 8, 1)';
+    ctx.fillRect(platform.x, platform.y, platform.w, platform.h);
+    ctx.restore();
+
+    // ring
+    ctx.drawImage(ringImage, ring.x, ring.y, ring.w, ring.h);
 };
 
 function update() {
+    if (ball.start) {
 
+        if (ball.y + ball.h >= platform.y) {
+            ball.dy = -ball.dy;
+            ball.y += ball.dy;
+        } else {
+            ball.y += ball.dy;
+            ball.dy *= ball.g;
+        }
+    }
+};
+
+function rebound() {
+    ball.start = true;
+    ball.y = 50;
 };
 
 
-
-
+/* 
 function makeEaseOut(timing) {
     return function (timeFraction) {
         return 1 - timing(1 - timeFraction);
@@ -73,13 +117,13 @@ function quad(timeFraction) {
 }
 
 function rebound() {
-    let to = canvas.height - (ball.h + 200);
+    let to = canvas.height - (ball.h + 100);
     let width = 1200;
     animate({
         duration: 3000,
         timing: makeEaseOut(bounce),
         draw(progress) {
-            ball.y = to * progress + 200;
+            ball.y = to * progress + 100;
         }
     });
     animate({
@@ -110,6 +154,6 @@ function animate(options) {
     });
 };
 
-
+ */
 
 window.onload = () => playGame(); 
